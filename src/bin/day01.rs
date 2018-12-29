b37f9660798125434aa75166d75bb01621dfa3ee
steve <meadsteve@gmail.com>
2018-12-29 23:13:21 +0100

    broken day 01 part 02

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,32 +2,71 @@ use std::io;
 use std::io::Read;
 use std::str::Lines;
 use std::io::Write;
+use std::iter::Map;
+use std::collections::HashSet;
 
-type Result<T> = ::std::result::Result<T, Box<::std::error::Error>>;
+type ExecutionResult<T> = Result<T, Box<::std::error::Error>>;
 
-fn main() -> Result<()> {
+fn main() -> ExecutionResult<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    part1(input.lines())?;
+    part1(input.clone().lines())?;
+    part2(input.clone().lines())?;
     Ok(())
 }
 
-fn part1(input: Lines) -> Result<()> {
+fn part1(input: Lines) -> ExecutionResult<()> {
     let result = sum_list(input);
     writeln!(io::stdout(), "Part One: {}", result)?;
     Ok(())
 }
 
-fn sum_list(numbers: Lines) -> i32{
-    return numbers
-        .map(|num_string| num_string.parse::<i32>().unwrap())
+fn part2(input: Lines) -> ExecutionResult<()> {
+    let result = first_repeated(input);
+    match result {
+        Ok(i) => writeln!(io::stdout(), "Part Two: {}", i)?,
+        Err(e) => writeln!(io::stdout(), "Part Two went wrong: {}", e)?
+    }
+    Ok(())
+}
+
+fn sum_list(lines: Lines) -> i32 {
+    return cast_lines_to_numbers(lines)
         .fold(0, |total, x| total + x);
 }
 
+fn first_repeated(lines: Lines) -> Result<i32, &'static str>{
+    let mut totals = HashSet::new();
+    let mut total = 0;
+    totals.insert(total);
+
+    for number in cast_lines_to_numbers(lines) {
+        total = total + number;
+        if totals.contains(&total) {
+            return Ok(total);
+        }
+        totals.insert(total);
+    }
+    return Err("no total repeated");
+}
+
+fn cast_lines_to_numbers(lines: Lines) -> Map<Lines, fn(&str) -> i32> {
+    return lines
+        .map(|num_string| num_string.parse::<i32>().unwrap());
+}
 
 #[test]
 fn it_sums_a_list() {
     let test_input = "1\n2\n1\n-1";
     assert_eq!(3, sum_list(test_input.lines()))
+}
+
+#[test]
+fn it_finds_the_first_repeated_intermediate_total() {
+    let test_input = "1\n2\n2\n-4\n2";
+//                         1  3  5   1  3
+    let result = first_repeated(test_input.lines());
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result.unwrap_or_default(), 1)
 }
